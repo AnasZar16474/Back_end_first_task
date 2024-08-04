39fd1d5110448952bf7c9a0e5f8554ae22324fd9
Anas <zarzouranas@gmail.com>
2024-08-04 13:13:53 +0300

    final project11

---
+++ ConsoleApp1/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6B8RVV6;Database=task;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
